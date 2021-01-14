95e9de6d8fbe31884a2ea78f9a1c149330b10662
Travis Peterson <contact@travispeterson.us>
2021-01-14 10:51:10 -0600

    fix: removes form8ion dependency

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-module.exports = {extends: '@form8ion/cypress'};
+module.exports = {
+  plugins: [
+    'chai-friendly',
+    'mocha'
+  ],
+
+  extends: [
+    'plugin:cypress/recommended',
+    'plugin:mocha/recommended'
+  ],
+
+  rules: {
+    'no-unused-expressions': 'off',
+    'chai-friendly/no-unused-expressions': 'error',
+    'mocha/no-skipped-tests': 'off',
+    'mocha/no-mocha-arrows': 'off',
+    'mocha/no-setup-in-describe': 'off'
+  }
+};
